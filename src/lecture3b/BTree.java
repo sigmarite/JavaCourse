af6a059be92be2bdcd80690b37b5a2e9d60fbe0c
Marco Pividori <pividori@esteco.com>
2016-08-22 14:29:10 +0200

    Add BTree abstract class
    BTree stands for Binary Tree, and contains all the properties required by a binary tree.
     - isEmpty()   checks for emptiness and returns boolean
     - getLabel()  returns the label
     - getRight()  returns either right or left depending trees
     - getLeft()

---
+++ src/lecture3b/BTree.java
@@ -0,0 +1,11 @@
+package lecture3b;
+
+/**
+ * Created by mpividori on 22/08/16.
+ */
+public abstract class BTree {
+    public abstract boolean isEmpty();
+    public abstract String getLabel();
+    public abstract BTree getRight();
+    public abstract BTree getLeft();
+}
