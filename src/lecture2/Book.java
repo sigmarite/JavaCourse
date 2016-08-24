b0c61ea835943d62a989fe34524f52032d706edd
Marco Pividori <pividori@esteco.com>
2016-08-24 14:22:25 +0200

    Minor changes in Book class (lecture2) in order to prepare for lecture4
    Implemente equals() and toString() methods.
    Change all the members to private.

--- src/lecture2/Book.java
+++ src/lecture2/Book.java
@@ -1,14 +1,16 @@
 package lecture2;
 
+import java.util.Arrays;
+
 /**
  * Created by mpividori on 11/08/16.
  */
 public class Book {
-    String title;
-    String author;
-    int numOfPages;
-    String[] topics;
-    String ISBN;
+    private String title;
+    private String author;
+    private int numOfPages;
+    private String[] topics;
+    private String ISBN;
 
     public Book(String title, String author, int numOfPages, String[] topics, String ISBN) {
         this.title = title;
@@ -16,6 +18,25 @@ public class Book {
         this.numOfPages = numOfPages;
         this.topics = topics;
         this.ISBN = ISBN;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", numOfPages=" + numOfPages +
+                ", topics=" + Arrays.toString(topics) +
+                ", ISBN='" + ISBN + '\'' +
+                '}';
+    }
+
+    public boolean equals(Book other) {
+        return title.equals(other.title) &&
+                author.equals(other.author) &&
+                numOfPages == other.numOfPages &&
+                Arrays.equals(topics, other.topics) &&
+                ISBN.equals(other.ISBN);
     }
 
     public String getTitle() {
