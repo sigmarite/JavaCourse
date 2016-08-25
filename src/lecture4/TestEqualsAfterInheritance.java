96c834dc43f5ce22259201fb3ba0b6dae9586305
Marco Pividori <pividori@esteco.com>
2016-08-25 12:33:05 +0200

    Modify the equals() method in the Book class and subclasses and update the TestEqualsAfterInheritance class
    The choice for equals() methods is to have two different methods: a standard o1.equals(o2) method that returns true only if o1 and o2 are members of the same class and all the equalities holds. A class method CLASS.equals(o1, o2) that simply checks whether o1 and o2 are instances of CLASS and then checks for equalities. In this way it is possible to query if two objects are equals using different criteria, while maintaining the reflectivity property in the usual o1.equals(o2) method.

--- src/lecture4/TestEqualsAfterInheritance.java
+++ src/lecture4/TestEqualsAfterInheritance.java
@@ -12,11 +12,14 @@ public class TestEqualsAfterInheritance {
         Book b1 = new Book("Titolo", "Autore", 101, "A123");
         ScientificBook sb1 = new ScientificBook("Titolo", "Autore", 101, null, "A123", "JAVA");
 
-        System.out.println("sb1.equals(b1) ->\t" + sb1.equals(b1));                 //EXPECTED true  (usa l'equals di Book perché non c'è un metodo equals Book!)
-        System.out.println("sb1.superEquals(b1) ->\t" + sb1.superEquals(b1));       //EXPECTED true
-        System.out.println("b1.equals(sb1) ->\t" + b1.equals(sb1));                 //EXPECTED true
+        System.out.println("sb1.equals(b1) ->\t" + sb1.equals(b1));                 //EXPECTED false (classi diverse!)
+        System.out.println("sb1.superEquals(b1) ->\t" + sb1.superEquals(b1));       //EXPECTED false
+        System.out.println("b1.equals(sb1) ->\t" + b1.equals(sb1));                 //EXPECTED false
         System.out.println("Object.equals(b1, sb1) ->\t" + (Objects.equals(b1, sb1)));  //??
         System.out.println("Object.equals(sb1, b1) ->\t" + (Objects.equals(sb1, b1)));  //??
+
+        System.out.println("Book.equals(b1, sb1) ->\t" + Book.equals(b1, sb1));     //EXPECTED true
+        System.out.println("ScientificBook.equals(b1, sb1) ->\t" + ScientificBook.equals(b1, sb1));     //EXPECTED false
 
     }
 }
