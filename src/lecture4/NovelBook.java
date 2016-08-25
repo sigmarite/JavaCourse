3b20606a34dbd44b081397b53afa4c5ab7a64c97
Marco Pividori <pividori@esteco.com>
2016-08-25 09:23:27 +0200

    Add NovelBook class

---
+++ src/lecture4/NovelBook.java
@@ -0,0 +1,37 @@
+package lecture4;
+
+import lecture2.Book;
+
+import java.util.Arrays;
+
+/**
+ * Created by mpividori on 25/08/16.
+ */
+
+public class NovelBook extends Book {
+
+    String[] topics;
+
+    public NovelBook(String title, String author, int numOfPages, String ISBN, String[] topics) {
+        super(title, author, numOfPages, ISBN);
+        this.topics = topics;
+    }
+
+    public NovelBook(Book other, String[] topics) {
+        super(other);
+        this.topics = topics;
+    }
+
+    public boolean equals(NovelBook other) {
+        return super.equals(other) &&
+                Arrays.equals(topics, other.topics);
+    }
+
+    public String[] getTopics() {
+        return topics;
+    }
+
+    public void setTopics(String[] topics) {
+        this.topics = topics;
+    }
+}
