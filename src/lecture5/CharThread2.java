02ee2cbc70c3514ebb608961ffdb91798af36bfb
Marco Pividori <pividori@esteco.com>
2016-08-25 17:56:49 +0200

    Add CharThread2 class

--- src/lecture5/CharThread2.java
+++ src/lecture5/CharThread2.java
@@ -3,21 +3,21 @@ package lecture5;
 /**
  * Created by mpividori on 25/08/16.
  */
-
-class CharThread1 extends Thread { char c;
-    CharThread1(char aChar) {
-        c = aChar; }
+public class CharThread2 implements Runnable {
+    char c;
+    CharThread2(char aChar) {
+        c = aChar;
+    }
     public void run() {
         while (true) {
             System.out.println(c);
             try {
-                sleep(100);
+                Thread.sleep(100);
             }
             catch (InterruptedException e) {
                 System.out.println("Interrupted");
                 return;
             }
-
         }
     }
 }
