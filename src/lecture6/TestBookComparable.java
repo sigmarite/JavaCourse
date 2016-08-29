eb2b29f9ac7f41040f50f82938bfd1f78cbe0eeb
Marco Pividori <marpiv_prog@yahoo.it>
2016-08-29 13:57:33 +0200

    Add TestBookComparable class

--- src/lecture6/TestBookComparable.java
+++ src/lecture6/TestBookComparable.java
@@ -2,6 +2,7 @@ package lecture6;
 
 import lecture2.Book;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,12 +14,25 @@ public class TestBookComparable {
     public static void main(String[] args) {
         List<Book> library = new LinkedList<>();
         library.add(new Book("Abc", "cde", 100, "A123"));
-        library.add(new Book("Abc", "cde", 100, "A123"));
-        library.add(new Book("Abc", "cde", 100, "A123"));
-        library.add(new Book("Abc", "cde", 100, "A123"));
-        library.add(new Book("Abc", "cde", 100, "A123"));
-        library.add(new Book("Abc", "cde", 100, "A123"));
-        library.add(new Book("Abc", "cde", 100, "A123"));
+        library.add(new Book("Abc", "Cde", 123, "A123"));
+        library.add(new Book("ee", "cde", 100, "A123"));
+        library.add(new Book("z", "cde", 100, "A123"));
+        library.add(new Book("ee", "cde", 12, "A123"));
+        library.add(new Book("ee", "cde", 111, "A123"));
+        library.add(new Book("ee", "dd", 100, "A123"));
+
+        System.out.println("UNSORTED LIBRARY");
+        for (Book b : library) {
+            System.out.println(b);
+        }
+
+        Collections.sort(library);
+
+        System.out.println("SORTED LIBRARY");
+        for (Book b : library) {
+            System.out.println(b);
+        }
+
 
     }
 }
