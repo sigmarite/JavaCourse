3307faf5d23ce516c3bd31b77783acae2f137565
Marco Pividori <marpiv_prog@yahoo.it>
2016-09-01 16:52:26 +0200

    Create a simple applet

---
+++ src/applets/SimpleApplet.java
@@ -0,0 +1,10 @@
+package applets;
+
+import java.awt.*;
+import java.applet.*;
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Primo passo per l'app per organizzare eventi!", 20, 20);
+    }
+}
